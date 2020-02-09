4f4d13be8260c19e1545c358ac618d5fe932c4f5
gbroiles <gbroiles@gmail.com>
2020-02-09 13:25:11 -0800

    working w command line params

--- clid.py
+++ clid.py
@@ -3,26 +3,38 @@ import requests
 import argparse
 import os,sys
 
-def start(DID):
+def create_parse():
+    parser = argparse.ArgumentParser(
+        description='Caller ID lookup tool')
+    parser.add_argument('subject', help='phone number[s] to look up', nargs='*')
+    return parser
+
+
+def start():
+    parser = create_parse()
+    args = parser.parse_args()
+    subject=args.subject
+    for i in subject:
+        print(process(i))
+
+def process(subject):
+    DID=str(subject)
     url = 'https://cnam.bulkCNAM.com/?id='+apikey+'&did='+DID
 #    print(url)
     r = s.get(url)
     status = r.status_code
-#    print(status)
     if (status != 200):
         print(status)
         print(url)
         r.raise_for_status()
-    print(r.text)
+    return(r.text)
 
-BULKAPI=''
 try:
     apikey = os.environ["BULKAPI"]
 except KeyError:
     apikey = 'NONE'
 
 s = requests.Session()
-DID = '9517884141'
 
 if __name__ == '__main__':
-    start(DID)
+    start()
