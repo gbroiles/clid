089524bff35cca74c22b6606593dd4aaa0361fc4
gbroiles <gbroiles@gmail.com>
2020-03-01 18:45:26 -0800

    add length check to phone #, set GO button as return/default action

--- wclid.py
+++ wclid.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
-# pylint: disable=missing-module-docstring,missing-function-docstring,invalid-name,no-member,unused-argument,unused-variable,missing-class-docstring
+""" GUI utility to retrieve Caller ID info given a phone number """
+# pylint: invalid-name,no-member
 import os
 import requests
 import wx
@@ -7,7 +8,9 @@ import clid
 
 
 class mainWindow(wx.Frame):
+    """ Main window """
     def __init__(self, parent, title):
+        """ set up defaults """
         super(mainWindow, self).__init__(
             parent, title=title, style=wx.DEFAULT_FRAME_STYLE, size=(300, 300)
         )
@@ -15,13 +18,14 @@ class mainWindow(wx.Frame):
         self.InitUI()
 
     def InitUI(self):
+        """ initialize UI elements """
         menubar = wx.MenuBar()
         fileMenu = wx.Menu()
         #        importItem = fileMenu.Append(wx.ID_ANY, 'Import file for lookup')
         fileItem = fileMenu.Append(wx.ID_EXIT, "Exit", "Exit application")
         menubar.Append(fileMenu, "&File")
         self.SetMenuBar(menubar)
-        self.Bind(wx.EVT_MENU, self.OnQuit, fileItem)
+        self.Bind(wx.EVT_MENU, self.onQuit, fileItem)
         #       self.Bind(wx.EVT_MENU, self.Import, importItem)
         self.CreateStatusBar()
 
@@ -35,6 +39,7 @@ class mainWindow(wx.Frame):
         sizer.Add(self.tc, pos=(1, 0), flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
 
         btn1 = wx.Button(panel, label="Go")
+        btn1.SetDefault()
         sizer.Add(btn1, pos=(2, 0), flag=wx.ALL, border=5)
 
         self.check = wx.CheckBox(panel, label="Remote lookup")
@@ -48,30 +53,32 @@ class mainWindow(wx.Frame):
 
         panel.SetSizer(sizer)
 
-        self.Bind(wx.EVT_BUTTON, self.Process, id=btn1.GetId())
+        self.Bind(wx.EVT_BUTTON, self.process, id=btn1.GetId())
         if apikey != "NONE":
             self.check.SetValue(True)
         else:
             self.check.SetValue(False)
 
-    def OnQuit(self, e):
+    def onQuit(self, e):
+        """ user has requested close """
         self.Close()
 
-    def Process(self, e):
+    def process(self, e):
+        """ process user input, return value """
         dirty = self.tc.GetValue()
         clean = clid.cleanup(dirty)
         statusline = ""
         #        statusline = ('{}:{}:'.format(dirty,clean))
         self.tc.SetValue(clean)
         sb = self.GetStatusBar()
-        if self.check.GetValue() and apikey != "NONE":
+        if self.check.GetValue() and apikey != "NONE" and len(clean) == 10:
             sb.SetStatusText("Checking...")
             answer, status = clid.process(apikey, session, clean)
             statusline += str(status)
             line = clean + " = " + answer + "\n"
         else:
-            statusline += "Remote check disabled."
-            line = clean + "\n"
+            statusline += "Remote check disabled/invalid."
+            line = dirty + "/" + clean + " Not checked.\n"
         self.tc2.AppendText(line)
         self.tc.SetValue("")
         sb.SetStatusText(statusline)
@@ -89,6 +96,7 @@ session = requests.Session()
 
 
 def main():
+    """ main event loop """
     app = wx.App()
     frame = mainWindow(None, title="Caller ID Lookup")
     frame.Show()
